c5bdba1d42c3a824bbf73e6de2440568a3b9178f
胡殿凯 <843132974@qq.com>
2021-04-05 20:00:34 +0800

    import problem

--- main/main.go
+++ main/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"Golearn/Learn"
 	"fmt"
 )
 
 
 func main()  {
 	fmt.Println("ada")
-
 	Liming := Learn.Student{ 	Age: 12, 	Sex: "Man", 	Num: 1, }
+	Liming.RML.RLock()
+	fmt.Println(Liming)
+	Learn.ReadandRead(&Liming)
+	defer Liming.RML.RUnlock()
+	//Learn.LockandRead(&Liming)
 }
 
